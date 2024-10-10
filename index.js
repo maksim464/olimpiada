357642d934b67ae3c22c0662972072a54d5e61ad
maksim464 <145909522+maksim464@users.noreply.github.com>
2024-10-10 15:06:07 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("moreInfoBtn").addEventListener("click", function() {
+    alert("Дякуємо за інтерес до нашої школи! Більше інформації доступно на сторінці 'Про нас'.");
+});
+
+document.getElementById("contactForm").addEventListener("submit", function() {
+    ;
+    alert("Ваше повідомлення надіслано! Ми зв'яжемося з вами найближчим часом.");
+});
