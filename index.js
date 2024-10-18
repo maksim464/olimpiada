3eb7ea8802c49c761ec6c886432c3947248fd6f0
maksim464 <145909522+maksim464@users.noreply.github.com>
2024-10-18 08:56:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,4 @@ document.getElementById("contactForm").addEventListener("submit", function() {
     alert("Ваше повідомлення надіслано! Ми зв'яжемося з вами найближчим часом.");
 });
 
-window.onload = function() {
-        // Перевіряємо, чи користувач зайшов з мобільного пристрою
-        if (/Mobi|Android/i.test(navigator.userAgent)) {
-            alert("Зайдіть будь ласка з комп'ютера");
-        }
-    }
+
